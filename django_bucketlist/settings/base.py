fc30c40a357a152e6adbc53bd349951a94e5fde2
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-24 18:17:59 +0100

    (chore) deploy to heroku

--- django_bucketlist/settings/base.py
+++ django_bucketlist/settings/base.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'components')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
