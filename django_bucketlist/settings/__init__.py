e468667576dadbb7d70c0510c6748385c1d9ce0d
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-24 21:57:54 +0100

    (chore) add bower root config to deploy

--- django_bucketlist/settings/__init__.py
+++ django_bucketlist/settings/__init__.py
@@ -1,8 +1,9 @@
-import os
-if not os.getenv('TRAVIS') and not os.getenv('HEROKU'):
-    from django_envie.workroom import convertfiletovars
-    convertfiletovars()
-    from development import *
+# import os
+# if not os.getenv('TRAVIS') and not os.getenv('HEROKU'):
+#     from django_envie.workroom import convertfiletovars
+#     convertfiletovars()
+#     from development import *
 
-if os.getenv('HEROKU') is not None:
-    from production import *
+# if os.getenv('HEROKU') is not None:
+
+from production import *
