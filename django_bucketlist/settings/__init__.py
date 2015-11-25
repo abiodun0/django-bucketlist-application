99ac597cb0619fede1363758d6d230b506651050
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-25 04:22:21 +0100

    (chore) deploying

--- django_bucketlist/settings/__init__.py
+++ django_bucketlist/settings/__init__.py
@@ -1,12 +1,12 @@
 import os
-if not os.getenv('TRAVIS') and not os.getenv('HEROKU'):
-    from django_envie.workroom import convertfiletovars
-    convertfiletovars()
-    try:
-        from development import *
-    except ImportError:
-        from base import *
-        print "Using default database configuration"
+# if not os.getenv('TRAVIS') and not os.getenv('HEROKU'):
+#     from django_envie.workroom import convertfiletovars
+#     convertfiletovars()
+#     try:
+#         from development import *
+#     except ImportError:
+#         from base import *
+#         print "Using default database configuration"
 
-if os.getenv('HEROKU') is not None:
-    from production import *
+# if os.getenv('HEROKU') is not None:
+from production import *
