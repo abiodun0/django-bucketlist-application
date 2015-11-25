c97d672dd15c11277b2a77f473cd2f3488b420ea
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-25 16:22:17 +0100

    (fix) drop user table and fix some css issues

--- django_bucketlist/settings/development.py
+++ django_bucketlist/settings/development.py
@@ -1,12 +1,20 @@
 from .base import *
+import sys
 
-DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME' : 'bucketlist',
-      'USER' : 'bucketlist_user',
-      'PASSWORD': 'abcdef',
-      'HOST': '127.0.0.1',
-      'PORT': '5432',
-  }
-}
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'testdatabase',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'bucketlist',
+            'USER': 'bucketlist_user',
+            'PASSWORD': 'abcdef',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }}
