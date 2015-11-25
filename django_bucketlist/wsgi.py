a343c56dd94c748798017181d9291992d5ffbae1
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-25 05:46:56 +0100

    (chore) deploying

--- django_bucketlist/wsgi.py
+++ django_bucketlist/wsgi.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_bucketlist.settings")
@@ -19,4 +18,3 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-application = Cling(get_wsgi_application())
