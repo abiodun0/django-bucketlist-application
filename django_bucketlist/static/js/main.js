2caa0c2eb607067161f9b5ce4de06d626ccbaea8
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-11-28 09:49:35 +0100

    (fix) fixes to naming of bucketlist dashboard/main.js

--- django_bucketlist/static/js/main.js
+++ django_bucketlist/static/js/main.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-    $('.edit-bucketitem').click(function(e){
+    $('.edit-bucketitem').click(function(e) {
         $(this).closest('.bucket-item').find('form').toggle();
         $(this).closest('.bucket-item').find('.bucket-item-content').toggle();
     })
-    $('.bucket-link').hover(function(e){
+    $('.bucket-link').hover(function(e) {
 
         $(this).tooltip('show');
-        
-    })
-    $('.bucket-link').click(function(e){
+
+    }).click(function(e) {
         e.preventDefault();
+        var bucketlist_name = $(this).closest('.panel').find('.panel-heading').text();
         $("#itemModal").modal("show");
         var url = $(this).data('action')
-        $("#itemModal").find('form').attr('action',url);
+        $("#itemModal").find('.modal-title').text("Add new item to " + bucketlist_name);
+        $("#itemModal").find('form').attr('action', url);
     });
-    $('.delete-bucketitem').click(function(e){
+    $('.delete-bucketitem').click(function(e) {
         e.preventDefault();
         $(this).popover({
-            container:false,
+            container: false,
             template: '<div class="popover" role="tooltip" style="width:300px;color:black">' +
-            '<div class="arrow"></div><h3 class="popover-title"></h3>'+
-            '<div class="popover-content"></div></div>',
+                '<div class="arrow"></div><h3 class="popover-title"></h3>' +
+                '<div class="popover-content"></div></div>',
             html: true,
         });
 
     });
-
-
 });
